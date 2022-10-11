07ac4f3f31c0f5ef65c108a4bfef01c2541a6b19
Peitra Erdi <peitrae@gmail.com>
2022-10-12 00:22:05 +0700

    Add basic class

---
+++ Data Structures/Linked List/SinglyLinkedList.js
@@ -0,0 +1,36 @@
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+	}
+}
+
+class SinglyLinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+
+	// TODO: push (val)
+
+	// TODO: pop ()
+
+	// TODO: shift ()
+
+	// TODO: unshift (val)
+
+	// TODO: get (index)
+
+	// set (val, index)
+
+	// insert (val, index)
+
+	// remove (index)
+
+	// reverse ()
+
+	// print ()
+}
+
+const list = new SinglyLinkedList();
