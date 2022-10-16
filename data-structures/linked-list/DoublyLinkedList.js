e63b90c54b66dcd8906eba46d31dbe25c98ca351
Peitra Erdi <peitrae@gmail.com>
2022-10-16 18:49:35 +0700

    Add base class

---
+++ data-structures/linked-list/DoublyLinkedList.js
@@ -0,0 +1,37 @@
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+		this.prev = null;
+	}
+}
+
+class DoublyLinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = null;
+	}
+
+  // push(val)
+
+  // pop()
+
+  // unshift(val)
+
+  // shift()
+
+  // get(index)
+
+  // set(val, index)
+
+  // insert(val, index)
+
+  // remove(index)
+
+  // reverse()
+
+  // toArray()
+}
+
+
